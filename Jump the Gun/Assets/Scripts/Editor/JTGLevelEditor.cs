fea7a18f1b2c6fd880ca17040cd12de935318c0f
amanthul <zhanghc.98@gmail.com>
2020-09-15 15:55:13 -0400

    Level editor window created

---
+++ Jump the Gun/Assets/Scripts/Editor/JTGLevelEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class JTGLevelEditor : EditorWindow
+{
+    [MenuItem("Window/JTG Level Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow<JTGLevelEditor>("JTG Level Editor");
+    }
+
+    private void OnGUI()
+    {
+        // Window code
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName != "LevelEditorTest")
+            return;
+
+        GUILayout.Label("Official 2D Level Editor for Jump The Gun", EditorStyles.miniLabel);
+
+        if (GUILayout.Button("Create a 2D Platform"))
+        {
+            Debug.Log("A platform has been created!");
+        }
+    }
+
+    #region Custom Methods
+
+    private void CreatePlatform()
+    {
+
+    }
+
+    #endregion
+}
