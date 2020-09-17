499d767710d8cd3e2944a8deefababf7d6c6b202
Gavin <gmd3659@rit.edu>
2020-09-17 18:07:21 -0400

    Camera following player added with Cinemachine

---
+++ Jump the Gun/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    Camera cam = Camera.main;
+    public GameObject player;
+
+    float camHeight;
+    float camWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        camHeight = 2f * cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y > camHeight/2)
+        {
+            //cam.transform = player.transform.position.y;
+        }
+    }
+}
