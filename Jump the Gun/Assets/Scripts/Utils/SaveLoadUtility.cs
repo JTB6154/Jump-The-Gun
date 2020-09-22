8cc826ba9bc4c5999288473b95f0389654d76aa7
AmanThuL <zhanghc.98@gmail.com>
2020-09-22 18:36:52 -0400

    Updated level geometry tool layout

---
+++ Jump the Gun/Assets/Scripts/Utils/SaveLoadUtility.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class SaveLoadUtility
+{
+    public static string ConvertToJsonString(object obj)
+    {
+        return JsonUtility.ToJson(obj, false);
+    }
+}
