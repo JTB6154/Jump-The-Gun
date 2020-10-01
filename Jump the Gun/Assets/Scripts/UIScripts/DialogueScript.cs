f32fd4c2b29f9deddfff2f9264e766e6b3b5da63
Gavin <gmd3659@rit.edu>
2020-10-01 18:37:36 -0400

    Dialogue Creation Started

---
+++ Jump the Gun/Assets/Scripts/UIScripts/DialogueScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    bool entered;
+    int timesEntered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entered = false;
+        timesEntered = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<BoxCollider2D>().IsTouching(player.GetComponent<BoxCollider2D>()) == true && entered == false)
+        {
+            entered = true;
+            StartDialogue();
+            timesEntered++;
+        }
+        else
+        {
+            //After being away from NPC for 15 seconds, they can then be triggered with next dialogue option
+            Invoke("ResetEntered", 10);
+        }
+    }
+
+    void ResetEntered()
+    {
+        entered = false;
+    }
+
+    void StartDialogue()
+    {
+        switch (timesEntered)
+        {
+            case 0:
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+}
