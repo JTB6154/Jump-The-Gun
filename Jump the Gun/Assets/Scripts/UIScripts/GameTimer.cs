e44511b3d7b26fe8bbe380fdc865ad1fd2e50f6b
Gavin <gmd3659@rit.edu>
2020-10-27 17:25:31 -0400

    Game Timer created and added to main scene

---
+++ Jump the Gun/Assets/Scripts/UIScripts/GameTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+
+    public Text timerText;
+    float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        //string hours = 
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        //Add extra 0s
+        if (t / 60 < 10)
+        {
+            minutes = "0" + minutes;
+        }
+
+        if (t % 60 < 10)
+        {
+            seconds = "0" + seconds;
+        }
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
