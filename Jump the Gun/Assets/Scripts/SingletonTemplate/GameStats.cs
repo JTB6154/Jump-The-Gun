6c53db5e390c4c3d80f9f2d83c8c29d4d8c3f62e
Gavin <gmd3659@rit.edu>
2020-10-29 17:11:41 -0400

    Small changes to scripts to help with saving/loading

--- Jump the Gun/Assets/Scripts/SingletonTemplate/GameStats.cs
+++ Jump the Gun/Assets/Scripts/SingletonTemplate/GameStats.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class GameStats : Singleton<GameStats>
 {
+    //Check if paused
+    public bool isPaused = false;
 
+    //Variables for saving and loading 
+    public float gameTime = 0f;
+    public int bigRecoilAmmo = 2;
+    public int rocketLauncherAmmo = 2;
+    public float playerPosX = 0f;
+    public float playerPosY = 0f;
+    public float playerMomentumX = 0f;
+    public float playerMomentumY = 0f;
 }
