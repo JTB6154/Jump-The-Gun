7cdafce9f0cbb00ed2db419d9f0d08f4bd8b166d
JTB6154 <jtb6154@rit.edu>
2020-11-03 17:43:04 -0500

    Completed Controls Reassignment
    Now controls can be reassigned in the scene. and will persist through scenes since they are saved in playerprefs

--- Jump the Gun/Assets/Scripts/SingletonTemplate/Options.cs
+++ Jump the Gun/Assets/Scripts/SingletonTemplate/Options.cs
@@ -4,21 +4,93 @@ using UnityEngine;
 
 public class Options : Singleton<Options>
 {
-    public KeyCode Left = KeyCode.A;
-    public KeyCode Right = KeyCode.D;
-    public KeyCode Fire1 = KeyCode.Mouse0;
-    public KeyCode Fire2 = KeyCode.Mouse1;
+    public KeyCode Left { get; set; }
+    public KeyCode Right{ get; set; }
+    public KeyCode Fire1 { get; set; }
+    public KeyCode Fire2 { get; set; }
 
     private bool updatingKeyCode = false;
     private int updatingKeyOfIndex;
+    private Event keyEvent;
 
-    private void Update()
+    override protected void Awake()
+    {
+        base.Awake();
+
+        Left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("left", "A"));
+        Right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("right", "D"));
+        Fire1 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("fire1", "Mouse0"));
+        Fire2 = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("fire2", "Mouse1"));
+
+    }
+    private void OnGUI()
     {
         if (updatingKeyCode)
-        { 
+        {
             //get what key is being pushed and assign it to the key being updated
-            if(Input.
+            keyEvent = Event.current;
+            if (keyEvent.isKey || keyEvent.isMouse)
+            {
+                updatingKeyCode = false;
+                switch (updatingKeyOfIndex) {
+                    case 0:
+                        if (checkKeyCodeInUse(keyEvent))
+                        {
+                            if(keyEvent.isKey) Left = keyEvent.keyCode;
+                            if (keyEvent.isMouse) Left = ConvertMouseIntToKeycode(keyEvent.button);
+                            PlayerPrefs.SetString("left", Left.ToString());
+                        }
+                        break;
+                    case 1:
+                        if (checkKeyCodeInUse(keyEvent))
+                        {
+                            if (keyEvent.isKey) Right = keyEvent.keyCode;
+                            if (keyEvent.isMouse) Right = ConvertMouseIntToKeycode(keyEvent.button);
+                            PlayerPrefs.SetString("right", Right.ToString());
+                        }
+                        break;
+                    case 2:
+                        if (checkKeyCodeInUse(keyEvent))
+                        {
+                            if (keyEvent.isKey) Fire1 = keyEvent.keyCode;
+                            if (keyEvent.isMouse) Fire1 = ConvertMouseIntToKeycode(keyEvent.button);
+                            PlayerPrefs.SetString("fire1", Fire1.ToString()); 
+                        }
+                        break;
+                    case 3:
+                        if (checkKeyCodeInUse(keyEvent))
+                        {
+                            if (keyEvent.isKey) Fire2 = keyEvent.keyCode;
+                            if (keyEvent.isMouse) Fire2 = ConvertMouseIntToKeycode(keyEvent.button);
+                            PlayerPrefs.SetString("fire2", Fire2.ToString());
+                        }
+                        break;
+                }
+
+            }
+
         }
+    }
+
+    private bool checkKeyCodeInUse(Event key)
+    {
+        KeyCode code;
+        if (keyEvent.isKey)
+        {
+            code = key.keyCode;
+        }
+        else
+        {
+             code = ConvertMouseIntToKeycode(key.button);
+        }
+        return !(code == Right || code == Left || code == Fire1 || code == Fire2);
+
+    }
+
+
+    public KeyCode ConvertMouseIntToKeycode(int key)
+    {
+        return (KeyCode)System.Enum.Parse(typeof(KeyCode), "Mouse" + key);
     }
 
     public void UpdateKeyOfIndex(int index)
