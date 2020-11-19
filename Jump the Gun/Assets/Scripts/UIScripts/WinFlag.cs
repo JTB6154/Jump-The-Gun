34e1a2b10accece2f53fec910769dec5ef51b04b
Gavin <gmd3659@rit.edu>
2020-11-18 23:51:48 -0500

    Win condition created

---
+++ Jump the Gun/Assets/Scripts/UIScripts/WinFlag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinFlag : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == player.gameObject.name )
+        {
+            GameStats.Instance.finishTime = GameStats.Instance.previousTime;
+            Cursor.visible = true;
+            SceneManager.LoadScene("WinScene", LoadSceneMode.Single);
+            GameStats.Instance.SaveData();
+        }
+    }
+}
---
+++ Jump the Gun/Assets/Scripts/UIScripts/WinScreenTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreenTime : MonoBehaviour
+{
+    public Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText.text = "Total Time: " + GameStats.Instance.finishTime.ToString();
+    }
+}
