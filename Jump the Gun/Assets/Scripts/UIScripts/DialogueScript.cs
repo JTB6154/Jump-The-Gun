1d6d085f9454c5729078ecb13c55380f47e38e87
Gavin <gmd3659@rit.edu>
2020-11-18 20:06:25 -0500

    small changes to dialogue script

--- Jump the Gun/Assets/Scripts/UIScripts/DialogueScript.cs
+++ Jump the Gun/Assets/Scripts/UIScripts/DialogueScript.cs
@@ -19,8 +19,6 @@ public class DialogueScript : MonoBehaviour
     bool invoking;
     int timesEntered;
 
-    string current;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -60,30 +58,18 @@ public class DialogueScript : MonoBehaviour
     void StartDialogue()
     {
         //Clear dialogue box if text remains
-        //dialogueTextBox.GetComponent<Text>().text = "";
+        dialogueTextBox.GetComponent<Text>().text = "";
 
         //Choose dialogue
         if (timesEntered < dialogueOptions.Capacity - 1)
         {
             dialogueTextBox.GetComponent<Text>().text = dialogueOptions[timesEntered];
-
-            /*
-            current = "";
-            foreach (char letter in dialogueOptions[timesEntered])
-            {
-                DelayText(letter);
-            }*/
+            //DelayText(timesEntered);
         }
         else
         {
             dialogueTextBox.GetComponent<Text>().text = dialogueOptions[dialogueOptions.Capacity - 1];
-
-            /*
-            current = "";
-            foreach (char letter in dialogueOptions[dialogueOptions.Capacity - 1])
-            {
-                DelayText(letter);
-            }*/
+            //DelayText(dialogueOptions.Capacity - 1);
         }
 
         //Make dialogue visible
@@ -91,14 +77,28 @@ public class DialogueScript : MonoBehaviour
 
         //Adjust times entered
         timesEntered++;
-
     }
-    /*    FIX LATER
-    IEnumerator DelayText(char letter)
+
+    void DelayText(int index)
     {
-        yield return new WaitForSeconds(.2f);
-        current += letter;
-        dialogueTextBox.GetComponent<Text>().text = current;
-    }*/
+        string current = "";
+        float t = Time.time;
+
+        for (int i = 0; i < dialogueOptions[index].Length;)
+        {
+            if (Time.time >= t + .2f)
+            {
+                current += dialogueOptions[index].Substring(i, 1);
+                dialogueTextBox.GetComponent<Text>().text = current;
+                t = Time.time;
+                i++;
+            }
+            else
+            {
+                Debug.Log(Time.time);
+                Debug.Log(t);
+            }
+        }
+    }
 
 }
