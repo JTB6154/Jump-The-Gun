4f18609fc92b2fa3a529e2fd8e945198dd4b6c04
Gavin <gmd3659@rit.edu>
2020-11-24 18:08:23 -0500

    Small UI tweeks

--- Jump the Gun/Assets/Scripts/UIScripts/FullSaveReset.cs
+++ Jump the Gun/Assets/Scripts/UIScripts/FullSaveReset.cs
@@ -7,6 +7,17 @@ public class FullSaveReset : MonoBehaviour
     public void ResetAllSaveData()
     {
         //Reset all save data
+        GameStats.Instance.newGame = true;
+        GameStats.Instance.hasSaveData = 0;
+        GameStats.Instance.previousTime = 0f;
+        GameStats.Instance.hasBigRecoil = 0;
+        GameStats.Instance.hasRocketLauncher = 0;
+        GameStats.Instance.playerPosX = -5f;
+        GameStats.Instance.playerPosY = 77f;
+        GameStats.Instance.playerMomentumX = 0f;
+        GameStats.Instance.playerMomentumY = 0f;
+
+        //Reset all save data
         PlayerPrefs.SetInt("hasSaveData", 0);
         PlayerPrefs.SetFloat("previousTime", 0);
         PlayerPrefs.SetInt("bigRecoilAmmo", 0);
